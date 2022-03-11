26410e167c8da4789737f461fcb2b08d92778cda
Yuri <sanfordyuri@gmail.com>
2022-03-11 13:49:25 -0300

    Explorando POO em C#

---
+++ Course/Course/BankAccount.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class BankAccount
+    {
+
+        private double _amount;
+        private string _name;
+        private int _agencia;
+        private int _conta;
+
+        public BankAccount(String name, int agencia, int conta)
+        {
+            this._name = name;
+            this._agencia = agencia;
+            this._conta = conta;
+            this._amount = 0.0;
+        }
+
+
+        public Boolean checkAmountIsOk(double amount)
+        {
+            if (!(amount > 0.0))
+            {
+                return false;
+                throw new ArgumentException("A quantia deve ser maior do que R$ 0.0");
+            }
+
+            return true;
+        }
+
+        public Boolean deposit(double amount)
+        {
+            if (checkAmountIsOk(amount))
+            {
+                this._amount += amount;
+                return true;
+            }
+
+            return false;
+        }
+
+        public Boolean withdraw(double amount)
+        {
+            if (checkAmountIsOk(amount))
+            {
+                if (this._amount >= amount)
+                {
+                    this._amount -= amount;
+                    return true;
+                }
+                throw new ArgumentException("Saldo insuficiente");
+
+            }
+
+            return false;
+        }
+
+        public void transfer(double amount, BankAccount account)
+        {
+            withdraw(amount);
+            account.deposit(amount);
+        }
+
+        public Double getAmount()
+        {
+            return this._amount;
+        }
+
+        public String getOwner()
+        {
+            return this._name;
+        }
+
+
+    }
+}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -4,10 +4,24 @@
     {
         static void Main(string[] args)
         {
-            int result = somar(10, 55);
-            Console.WriteLine(result);
-            int[] numeros = {1,2,3,4,5,22,54,32,12,89,54};
-            listarNumerosPares(numeros);
+            //int result = somar(10, 55);
+            //Console.WriteLine(result);
+            //int[] numeros = {1,2,3,4,5,22,54,32,12,89,54};
+            //listarNumerosPares(numeros);
+            BankAccount bankAccount_Yuri = new BankAccount("Yuri", 0001, 112233);
+            BankAccount bankAccount_Mylena = new BankAccount("Mylena", 0002, 223344);
+
+            Console.WriteLine(bankAccount_Yuri.getOwner() + " - " + bankAccount_Yuri.getAmount());
+            Console.WriteLine(bankAccount_Mylena.getOwner() + " - " + bankAccount_Mylena.getAmount());
+
+            bankAccount_Yuri.deposit(60.0);
+            bankAccount_Yuri.withdraw(30);
+            Console.WriteLine(bankAccount_Yuri.getOwner() + " - " + bankAccount_Yuri.getAmount());
+            Console.WriteLine(bankAccount_Mylena.getOwner() + " - " + bankAccount_Mylena.getAmount());
+
+            bankAccount_Yuri.transfer(-10, bankAccount_Mylena);
+            Console.WriteLine(bankAccount_Yuri.getOwner() + " - " + bankAccount_Yuri.getAmount());
+            Console.WriteLine(bankAccount_Mylena.getOwner() + " - " + bankAccount_Mylena.getAmount());
         }
 
         public static int somar(int a, int b)
