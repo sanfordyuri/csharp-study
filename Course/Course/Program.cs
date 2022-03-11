745faf90619c10fde3817b3acf334080e0ccd5b4
Yuri <sanfordyuri@gmail.com>
2022-03-11 13:14:52 -0300

    Explorando o c#

---
+++ Course/Course/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = somar(10, 55);
+            Console.WriteLine(result);
+            int[] numeros = {1,2,3,4,5,22,54,32,12,89,54};
+            listarNumerosPares(numeros);
+        }
+
+        public static int somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void listarNumerosPares(int[] numeros)
+        {
+            foreach (int numero in numeros)
+            {
+                if ((numero % 2) == 0)
+                {
+                    Console.WriteLine(numero + " É par!");
+                }
+            }
+        }
+
+    }
+}
